909010ffc16c7ef68eaad98feaa6822655c2f742
Ryan Hammer <ryanhammer1@protonmail.com>
2024-01-27 10:55:11 -0500

    move component functions out to separate file

--- app/components/fractions-widget/index.tsx
+++ app/components/fractions-widget/index.tsx
@@ -1,17 +1,12 @@
 'use client';
 
-import { FormEvent, useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { motion } from 'framer-motion';
-import {
-  RocketLaunchIcon,
-  Cog6ToothIcon,
-  ArrowDownIcon,
-  ArrowRightIcon,
-  ArrowPathIcon,
-} from '@heroicons/react/20/solid';
+import { RocketLaunchIcon, Cog6ToothIcon } from '@heroicons/react/20/solid';
 import Image from 'next/image';
 import fractionsWithHammerImage from '../../../public/fractions_hammer.png';
 import { stepsText, StepText } from './lesson-steps';
+import { ContinueButton, NumericInputForm, OptionButtons, ResetButton } from './cta-buttons';
 import { InteractiveSquare } from './interactive-square';
 
 interface FractionsWidgetProps {
@@ -242,137 +237,6 @@ export const FractionsWidget = ({ className }: FractionsWidgetProps) => {
           )}
         </div>
       </div>
-    </div>
-  );
-};
-
-interface ContinueButtonProps {
-  currentStepIndex: number;
-  setCurrentStepIndex: React.Dispatch<React.SetStateAction<number>>;
-  setOptionChoice: React.Dispatch<React.SetStateAction<number>>;
-}
-
-const ContinueButton = ({
-  currentStepIndex,
-  setCurrentStepIndex,
-  setOptionChoice,
-}: ContinueButtonProps): JSX.Element => {
-  const buttonClickHandler = (): void => {
-    if (currentStepIndex === 6) setOptionChoice(0);
-
-    setCurrentStepIndex(currentStepIndex + 1);
-  };
-
-  return (
-    <div className='flex items-end'>
-      <button className='rounded-2xl bg-strong-blue text-white p-3' onClick={() => buttonClickHandler()}>
-        <ArrowDownIcon className='w-5 h-5' />
-      </button>
-    </div>
-  );
-};
-
-interface NumericInputFormProps extends ContinueButtonProps {
-  setOptionChoice: React.Dispatch<React.SetStateAction<number>>;
-}
-
-const NumericInputForm = ({ currentStepIndex, setCurrentStepIndex, setOptionChoice }: NumericInputFormProps) => {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    const numericValue = parseInt(inputValue, 10);
-    if (numericValue === 1) {
-      setOptionChoice(1);
-      setCurrentStepIndex(6);
-    } else {
-      setOptionChoice(0);
-      setCurrentStepIndex(currentStepIndex + 1);
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit} className='flex w-full justify-between text-white'>
-      <input
-        id='numericInput'
-        type='number'
-        value={inputValue}
-        placeholder='Enter a number...'
-        onChange={(e) => setInputValue(e.target.value)}
-        required
-        className='hideNumberInputArrows rounded-lg border border-strong-blue bg-strong-blue bg-opacity-30 pl-3 py-2'
-      />
-      <button type='submit' className='rounded-2xl bg-strong-blue text-white p-3'>
-        <ArrowRightIcon className='w-5 h-5' />
-      </button>
-    </form>
-  );
-};
-
-interface OptionButtonsProps extends NumericInputFormProps {
-  options: string[];
-}
-
-const OptionButtons = ({
-  options,
-  setOptionChoice,
-  currentStepIndex,
-  setCurrentStepIndex,
-}: OptionButtonsProps): JSX.Element => {
-  const buttonClickHandler = (ind: number): void => {
-    setOptionChoice(ind);
-    setCurrentStepIndex(currentStepIndex + 1);
-  };
-
-  return (
-    <>
-      <div className='flex flex-col gap-y-3'>
-        {options.map((option, ind) => (
-          <button
-            className='rounded-2xl border border-strong-blue bg-strong-blue bg-opacity-30 text-white text-lg p-3 pr-10 w-fit max-w-[80%]'
-            key={ind}
-            onClick={() => buttonClickHandler(ind)}
-          >
-            {option}
-          </button>
-        ))}
-      </div>
-    </>
-  );
-};
-
-interface ResetButtonProps {
-  setCurrentStepIndex: React.Dispatch<React.SetStateAction<number>>;
-  setLineCount: React.Dispatch<React.SetStateAction<number>>;
-  setCurrentPartIndex: React.Dispatch<React.SetStateAction<number>>;
-  setShouldDisplaySquare: React.Dispatch<React.SetStateAction<boolean>>;
-  fullLessonReset: boolean;
-}
-
-const ResetButton = ({
-  setCurrentStepIndex,
-  setLineCount,
-  setCurrentPartIndex,
-  setShouldDisplaySquare,
-  fullLessonReset,
-}: ResetButtonProps): JSX.Element => {
-  const buttonClickHandler = (): void => {
-    setLineCount(0);
-    setCurrentStepIndex(fullLessonReset ? 0 : 7);
-
-    if (fullLessonReset) {
-      setShouldDisplaySquare(false);
-      setCurrentPartIndex(0);
-    }
-  };
-
-  return (
-    <div className='flex items-end'>
-      <button className='flex gap-2 rounded-2xl bg-strong-blue text-white p-3' onClick={() => buttonClickHandler()}>
-        <ArrowPathIcon className='w-5 h-5' />
-        {fullLessonReset ? 'Reset Lesson' : 'Clear Square'}
-      </button>
     </div>
   );
 };
